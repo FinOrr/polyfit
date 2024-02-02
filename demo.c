66ae1c0d72c1a7d550529fcb2c4b3df2d004f1fa
Fin <finorr3@gmail.com>
2024-02-02 09:50:16 +0700

    Update demo.c
    Clarify the demo, remove redundant code

--- demo.c
+++ demo.c
@@ -9,17 +9,17 @@ int main(void) {
   ////////////////////////////////////////////////////
 
   // Step 1: Initialise the polynomial
-  // You'll need to define the order of the polynomial; this could be optimised
-  // in the future. For now we need to manually assign the degree ('order') of
-  // the polynomial In this example we want to fit an x^2 curve, so we use
-  // order=2.
+  // You'll need to define the degree of the polynomial. This will be optimised
+  // in the future. For now, we need to manually assign the degree ('order') of
+  // the polynomial In this example we want to fit an x^3 curve, so we use
+  // degree=2.
   int32_t degree = 3;
   Polynomial *poly = initPolynomial(degree);
 
   // Step 2: Fit the polynomial coefficients to your data.
   // You will need an input array of samples (x and y components), these are
-  // used used to fit the polynomial. In this demo, we're modelling an x^2
-  // parabola, so let's record the first 4 components
+  // used used to fit the polynomial. In this demo, we're modelling an x^3
+  // parabola, so let's record the first 4 components of this curve.
   float xData[] = {0, 2, 4, 5};
   float yData[] = {0, 8, 64, 125};
 
@@ -34,28 +34,18 @@ int main(void) {
     exit(EXIT_FAILURE);
   }
 
-  // Step 3: Now you can interpolate or extrapolate any point on the fitted
-  // curve! Let's imagine we're at point x=20, we'd expect the y co-ordinate to
-  // be 400. float x = 20; float y = evaluatePolynomial(poly, x);
-  // printf("Extrapolated with degree [%d].\nWhen x=20, we expect y=[%.2f].",
-  // degree, y);
-
+  // Step 3: Now you can interpolate or extrapolate any point on the curve.
+  // Demo output text showing extrapolation / interpolation of the x^3 curve.
   printf("Testing polynomial for various x values:\n");
   printf("| %-10s | %-10s |\n", "x", "y (y=x^2)");
   printf("|------------|------------|\n");
 
+  // To find a data point on the curve, pass the polynomial and the desired x-
+  // component to the evaluatePolynomial() function. Let's test 20 locations:
   for (float x = -10; x <= 10; x += 1) {
     float y = evaluatePolynomial(poly, x);
     printf("| %-10.2f | %-10.2f |\n", x, y);
   }
-
-  // Extrapolation example
-  float extrapolation_x = 20;
-  float extrapolation_y = evaluatePolynomial(poly, extrapolation_x);
-  printf("\nExtrapolation Result:\n");
-  printf("  Polynomial Degree: %d\n", degree);
-  printf("  Extrapolation Point: x=%.2f\n", extrapolation_x);
-  printf("  Expected y Value: %.2f\n", extrapolation_y);
 
   // Step 4: Free the memory when we're done to prevent memory leaks.
   freePolynomial(poly);
